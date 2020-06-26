0f83521bc37548d9635d4505154b44a508069fbe
hoanggaphan <phansihoang1998@gmail.com>
2020-06-26 21:36:08 +0700

    [1.0.0] Connect & Create item mongodb

---
+++ sh/env.example.sh
@@ -0,0 +1,11 @@
+# Config database enviroment variables
+export DB_CONNECTION=***
+export DB_HOST=***
+export DB_PORT=***
+export DB_NAME=***
+export DB_USERNAME=***
+export DB_PASSWORD=***
+
+# Config app enviroment variables
+export APP_HOSTNAME=***
+export APP_PORT=***
