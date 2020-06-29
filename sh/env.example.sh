b4d29c9dd713f6c492b20a7cc0d88e871a4ae810
hoanggaphan <phansihoang1998@gmail.com>
2020-06-29 15:37:49 +0700

    [1.0.0] Send mail confirm account

--- sh/env.example.sh
+++ sh/env.example.sh
@@ -9,3 +9,9 @@ export DB_PASSWORD=***
 # Config app enviroment variables
 export APP_HOSTNAME=***
 export APP_PORT=***
+
+# Config admin email account
+export MAIL_USER=***
+export MAIL_PASSWORD=***
+export MAIL_HOST=***
+export MAIL_PORT=***
