9e06d46670b5bb693447aba6fd84dc9a6da3470d
hoanggaphan <phansihoang1998@gmail.com>
2020-07-01 16:18:11 +0700

    [1.0.0] Update user frontend

---
+++ src/public/js/updateUser.js
@@ -0,0 +1,114 @@
+let userAvatar = null;
+let userInfo = {};
+let originAvatarSrc = null;
+
+function updateUserInfo() {
+  $("#input-change-avatar").bind("change", function () {
+    const fileData = $(this).prop("files")[0];
+    const math = ["image/png", "image/jpg", "image/jpeg"];
+    const limit = 1048576; // byte = 1MB
+
+    if ($.inArray(fileData.type, math) === -1) {
+      alertify.notify(
+        "Kiểu file Không hợp lệ, chỉ chấp nhận file png, jpg hoặc jpeg",
+        "error",
+        5
+      );
+      $(this).val(null);
+      return;
+    }
+
+    if (fileData.size > limit) {
+      alertify.notify("Ảnh có kích thước tối đa 1MB", "error", 5);
+      $(this).val(null);
+      return;
+    }
+
+    if (typeof FileReader !== "undefined") {
+      const imagePreview = $("#image-edit-profile");
+      imagePreview.empty();
+
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        $("<img>", {
+          src: e.target.result,
+          id: "user-modal-avatar",
+          class: "avatar img-circle",
+          alt: "avatar",
+        }).appendTo(imagePreview);
+      };
+      imagePreview.show();
+      fileReader.readAsDataURL(fileData);
+
+      let formData = new FormData();
+      formData.append("avatar", fileData);
+      userAvatar = formData;
+    } else {
+      alertify.notify(
+        "Trình duyệt của bạn không hỗ trợ FileReader",
+        "error",
+        5
+      );
+    }
+  });
+
+  $("#input-change-username").bind("change", function () {
+    userInfo.username = $(this).val();
+  });
+
+  $("#input-change-gender-male").bind("click", function () {
+    userInfo.gender = $(this).val();
+  });
+
+  $("#input-change-gender-female").bind("click", function () {
+    userInfo.gender = $(this).val();
+  });
+
+  $("#input-change-address").bind("change", function () {
+    userInfo.address = $(this).val();
+  });
+
+  $("#input-change-phone").bind("change", function () {
+    userInfo.phone = $(this).val();
+  });
+}
+
+$(document).ready(function () {
+  updateUserInfo();
+
+  originAvatarSrc = $("#user-modal-avatar").attr("src");
+
+  $("#input-btn-update-user").bind("click", function () {
+    if ($.isEmptyObject(userInfo) && !userAvatar) {
+      alertify.notify(
+        "Bạn phải thay đổi thông tin trước khi cập nhật",
+        "error",
+        5
+      );
+    }
+
+    $.ajax({
+      url: "/user/update-avatar",
+      type: "put",
+      cache: false,
+      contentType: false,
+      processData: false,
+      data: userAvatar,
+      success: function (response) {
+        
+      },
+      error: function (error) {
+        
+      }
+    })
+
+    // console.log(userAvatar);
+    // console.log(userInfo);
+  });
+
+  $("#input-btn-cancel-user").bind("click", function () {
+    userAvatar = null;
+    userInfo = {};
+    $("#user-modal-avatar").attr("src", originAvatarSrc);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -5,74 +5,74 @@ import configSession from "./config/session";
 import initRoutes from "./routes/web";
 import connectFlash from "connect-flash";
 import passport from "passport";
-import https from "https";
-import pem from "pem";
+// import https from "https";
+// import pem from "pem";
 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err;
-  }
-  // Init app
-  const app = express();
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//   if (err) {
+//     throw err;
+//   }
+//   // Init app
+//   const app = express();
 
-  // Connect to MongoDB
-  connectDB();
+//   // Connect to MongoDB
+//   connectDB();
 
-  // Config session
-  configSession(app);
+//   // Config session
+//   configSession(app);
 
-  // Config view engine
-  configViewEngine(app);
+//   // Config view engine
+//   configViewEngine(app);
 
-  // Enable post data for request
-  app.use(express.urlencoded({ extended: true }));
+//   // Enable post data for request
+//   app.use(express.urlencoded({ extended: true }));
 
-  // Enable flash messages
-  app.use(connectFlash());
+//   // Enable flash messages
+//   app.use(connectFlash());
 
-  // Config passport
-  app.use(passport.initialize());
-  app.use(passport.session());
+//   // Config passport
+//   app.use(passport.initialize());
+//   app.use(passport.session());
 
-  // Init all routes
-  initRoutes(app);
+//   // Init all routes
+//   initRoutes(app);
 
-  https
-    .createServer({ key: keys.serviceKey, cert: keys.certificate }, app)
-    .listen(process.env.APP_PORT, process.env.APP_HOSTNAME, () => {
-      console.log(
-        `App listening at https://${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`
-      );
-    });
-});
-
-// // Init app
-// const app = express();
-
-// // Connect to MongoDB
-// connectDB();
-
-// // Config session
-// configSession(app);
-
-// // Config view engine
-// configViewEngine(app);
-
-// // Enable post data for request
-// app.use(express.urlencoded({ extended: true }));
-
-// // Enable flash messages
-// app.use(connectFlash());
-
-// // Config passport
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Init all routes
-// initRoutes(app);
-
-// app.listen(process.env.APP_PORT, process.env.APP_HOSTNAME, () => {
-//   console.log(
-//     `App listening at http://${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`
-//   );
+//   https
+//     .createServer({ key: keys.serviceKey, cert: keys.certificate }, app)
+//     .listen(process.env.APP_PORT, process.env.APP_HOSTNAME, () => {
+//       console.log(
+//         `App listening at https://${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`
+//       );
+//     });
 // });
+
+// Init app
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Config session
+configSession(app);
+
+// Config view engine
+configViewEngine(app);
+
+// Enable post data for request
+app.use(express.urlencoded({ extended: true }));
+
+// Enable flash messages
+app.use(connectFlash());
+
+// Config passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Init all routes
+initRoutes(app);
+
+app.listen(process.env.APP_PORT, process.env.APP_HOSTNAME, () => {
+  console.log(
+    `App listening at http://${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`
+  );
+});
