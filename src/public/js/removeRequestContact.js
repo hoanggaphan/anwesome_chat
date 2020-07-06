a0ebf2398389080d90dac2a851e82c04c1b93ed6
hoanggaphan <phansihoang1998@gmail.com>
2020-07-06 13:54:08 +0700

    [1.0.0] Add contact realtime

--- src/public/js/removeRequestContact.js
+++ src/public/js/removeRequestContact.js
@@ -10,8 +10,21 @@ function removeRequestContact() {
           $("#find-user").find(`div.user-remove-request-contact[data-uid = ${targetId}]`).hide();
           $("#find-user").find(`div.user-add-new-contact[data-uid = ${targetId}]`).css("display", "inline-block");
           decreaseNumberNotifContact("count-request-contact-sent");
+
+          socket.emit("remove-request-contact", { contactId: targetId });
         }
       }
     })
   });
 }
+
+socket.on("response-remove-request-contact", function (user) {
+  $(".noti_content").find(`span[data-uid = ${user.id}]`).remove();
+
+  // Xóa ở modal tab yêu cầu kết bạn
+
+  decreaseNumberNotifContact("count-request-contact-received");
+
+  decreaseNumberNotification("noti_contact_counter");
+  decreaseNumberNotification("noti_counter");
+});
